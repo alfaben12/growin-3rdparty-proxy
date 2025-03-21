a2e2b04c51d4b6a38987c603dea010b63c75d9d0
Thariq Alfa <thariq.benriska@mandirisekuritas.co.id>
2025-03-21 15:45:35 +0700

    Initial commit

---
+++ main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"net/http/httputil"
+	"net/url"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	targetURL := os.Getenv("TARGET_URL")
+	proxyPort := os.Getenv("PROXY_PORT")
+
+	if targetURL == "" || proxyPort == "" {
+		log.Fatal("TARGET_URL or PROXY_PORT is missing from .env")
+	}
+
+	remote, err := url.Parse(targetURL)
+	if err != nil {
+		log.Fatal("Invalid TARGET_URL:", err)
+	}
+
+	proxy := httputil.NewSingleHostReverseProxy(remote)
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		r.Host = remote.Host
+		proxy.ServeHTTP(w, r)
+	})
+
+	log.Printf("Reverse proxy running on :%s, forwarding to %s", proxyPort, targetURL)
+	err = http.ListenAndServe(":"+proxyPort, nil)
+	if err != nil {
+		log.Fatal("Server failed:", err)
+	}
+}
